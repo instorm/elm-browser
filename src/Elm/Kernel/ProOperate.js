e7757961b7e6a9937c287122ca1356b72880c7a5
makoto yamagata <yamagata@clouds-inc.jp>
2022-09-27 21:43:25 +0900

    fixed False -> false

--- src/Elm/Kernel/ProOperate.js
+++ src/Elm/Kernel/ProOperate.js
@@ -92,7 +92,7 @@ var _ProOperate_spawnCommunication_pro2 = F3(function (elmConfig, toSuccessTask,
             }
         }
         var config = _ProOperate_elmConfigToJsConfig(elmConfig);
-        config.onetime = False
+        config.onetime = false;
         config.onEvent = onEvent;
         var touchPromise = new Promise((resolve, reject) => {
             function onEvent(eventCode, response) {
